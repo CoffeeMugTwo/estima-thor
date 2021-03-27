13845cd9977b5ecc78ffc54b5f96db12b34b931a
Dr. Thomas Kittler <thomas.kittler.01@gmail.com>
2021-03-27 10:50:36 +0100

    Feat: Add EstimaThor class with base functions
    Feat: Add positional argument to CLI for task estimation file.

--- estima_thor/cli.py
+++ estima_thor/cli.py
@@ -2,16 +2,28 @@
 import argparse
 import sys
 
+from estima_thor.estima_thor import EstimaThor
+
 
 def main():
     """Console script for estima_thor."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
+    parser.add_argument('task_estimations_file',
+                        help="CSV file containing the task estimations")
     args = parser.parse_args()
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "estima_thor.cli.main")
+    # Read task estimations
+    task_estimations_df = EstimaThor.read_task_estimations_csv(args.task_estimations_file)
+
+    # Initialize EstimaThor
+    estima_thor = EstimaThor(task_estimations_df)
+
+    # Get estimations
+    est_min, est_peak, est_max = estima_thor.get_project_estimations()
+
+    # Print estimations on screen
+    print(f"Min = {est_min} \nPeak = {est_peak} \nMax = {est_max}")
+
     return 0
 
 
--- estima_thor/estima_thor.py
+++ estima_thor/estima_thor.py
@@ -1,1 +1,57 @@
 """Main module."""
+
+from pathlib import Path
+
+import pandas as pd
+
+
+def test_function():
+    print("Hello World! asdfasdfsadf")
+    return
+
+class EstimaThor(object):
+    """
+    Main class of this tool
+    """
+
+    def __init__(self, task_estimations_df: pd.DataFrame):
+        """
+        Constructor for this class
+
+        Parameters
+        ----------
+        task_estimations_df : pd.DataFrame
+            Data frame containing the task and respective estimations
+        """
+
+        self.task_estimations_df = task_estimations_df
+
+        return
+
+    @classmethod
+    def read_task_estimations_csv(cls, path_to_file: Path) -> pd.DataFrame:
+        """
+        Read the task estimations form a csv file into a pandas data frame.
+
+        Parameters
+        ----------
+        path_to_file : Path
+            Path to file
+
+        Returns
+        -------
+        task_estimations_df : pd.DataFrame
+        """
+        task_estimations_df = pd.read_csv(path_to_file,
+                                          sep=',',
+                                          index_col=0)
+        return task_estimations_df
+
+    def get_project_estimations(self):
+        # ToDo: this is just a data flow test
+        total_min = self.task_estimations_df['5_qtl'].sum()
+        total_max = self.task_estimations_df['95_qtl'].sum()
+        total_peak = self.task_estimations_df['peak'].sum()
+        return total_min, total_peak, total_max
+
+
